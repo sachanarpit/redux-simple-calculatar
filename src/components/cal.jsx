9ed93eed4a9373d489827158e8b37aec85e7862a
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-23 21:20:48 +0530

    css and jsx added

--- src/components/cal.jsx
+++ src/components/cal.jsx
@@ -2,6 +2,15 @@ export const Calculator = () => {
   return (
     <div>
       <h1>Simple Calculatar</h1>
+      <div>
+        <button>
+          <span>-</span>
+        </button>
+        <input type="text" name="" id="" value="0" />
+        <button>
+          <span>+</span>
+        </button>
+      </div>
     </div>
   );
 };
