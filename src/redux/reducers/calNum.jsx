4d908af7f953dc39b35d16c8f0a09d9e858f612f
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-23 21:29:38 +0530

    reducer are added

---
+++ src/redux/reducers/calNum.jsx
@@ -0,0 +1,10 @@
+export const calNum = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + action.payload;
+    case "DECREMENT":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
